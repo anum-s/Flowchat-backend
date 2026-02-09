09bcbe744d3157643f6f85a15e4df871f1887fa9
Anum <anums1113@gmail.com>
2026-02-09 14:35:27 +0500

    changing the cors origin

--- Socket/socket.js
+++ Socket/socket.js
@@ -8,8 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-       origin: process.env.NODE_ENV === 'production'?
-        process.env.FRONTEND_URL || '*': 'http://localhost:5173',
+       origin: true ,
        credentials:true ,
        methods:["GET","POST"]
     }
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ dotenv.config()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : 'http://localhost:5173',
+    origin: true,
     credentials: true
 }))
 
